85a4b10dfabd78cc8ba415be025c665407356531
empoci_k <empoci_k@epitech.eu>
2016-06-07 15:01:29 +0200

    Add lib network and client clean for Wilmot

---
+++ Network/includes/server.h
@@ -0,0 +1,29 @@
+/*
+** server.h for server in /Users/noboud_n/Documents/Share/PSU_2015_zappy/server/includes/
+**
+** Made by Nyrandone Noboud-Inpeng
+** Login   <noboud_n@epitech.eu>
+**
+** Started on  Tue Jun  7 11:49:04 2016 Nyrandone Noboud-Inpeng
+** Last update Tue Jun  7 13:21:35 2016 Nyrandone Noboud-Inpeng
+*/
+
+#ifndef SERVER_H_
+# define SERVER_H_
+
+typedef struct		s_data
+{
+  int			port;
+  int			world_x;
+  int			world_y;
+  int			max_clients;
+  int			delay;
+  char			**teams;
+}			t_data;
+
+/*
+** get_opt.c
+*/
+int	get_opt(int, char **, t_data *);
+
+#endif /* !SERVER_H_ */
---
+++ Network/src/main.c
@@ -0,0 +1,19 @@
+/*
+** main.c for main in /Users/noboud_n/Documents/Share/PSU_2015_zappy/serv/src
+**
+** Made by Nyrandone Noboud-Inpeng
+** Login   <noboud_n@epitech.net>
+**
+** Started on  Tue Jun  7 11:35:34 2016 Nyrandone Noboud-Inpeng
+** Last update Tue Jun  7 11:58:02 2016 Nyrandone Noboud-Inpeng
+*/
+
+#include "server.h"
+
+int		main(int argc, char **argv)
+{
+  t_data	data;
+
+  get_opt(argc, argv, &data);
+  return (0);
+}
