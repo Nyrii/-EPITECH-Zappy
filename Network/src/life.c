a6243e49c0c9aeb835b4526d7e956fd365978175
Nyrandone Noboud-Inpeng <noboud_n@epitech.eu>
2016-06-25 20:57:22 +0200

    Player now leaves every resources he has when he dies

--- Network/src/life.c
+++ Network/src/life.c
@@ -5,7 +5,7 @@
 ** Login   <nekfeu@epitech.net>
 **
 ** Started on  Thu Jun  9 01:10:25 2016 Kevin Empociello
-** Last update Sat Jun 25 19:53:56 2016 Nyrandone Noboud-Inpeng
+** Last update Sat Jun 25 20:56:54 2016 Nyrandone Noboud-Inpeng
 */
 
 #include <string.h>
@@ -17,10 +17,15 @@ int		player_life(t_server *srv, t_player *p)
 {
   struct timeb	now;
   double	tmp;
+  int		resource;
 
   ftime(&now);
   if (p->inventory[FOOD] <= 0)
     {
+      resource = 0;
+      while (srv->data.map && srv->data.map[p->y] && srv->data.map[p->y][p->x]
+	     && ++resource < NONE)
+	srv->data.map[p->y][p->x][resource] += p->inventory[resource];
       if (store_answer_p(p, strdup(DEAD), 0) == -1 || pdi(srv, p) == -1)
 	return (fprintf(stderr, ERR_BUFFER), -1);
       p->off = 1;
