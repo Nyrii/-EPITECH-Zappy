77c70d1a250030cafd8acafcca64825e7a169f67
Nyrandone Noboud-Inpeng <noboud_n@epitech.eu>
2016-06-25 14:13:38 +0200

    Modified the new_player handler

--- Network/src/new_player.c
+++ Network/src/new_player.c
@@ -5,7 +5,7 @@
 ** Login   <nekfeu@epitech.net>
 **
 ** Started on  Thu Jun  9 01:10:25 2016 Kevin Empociello
-** Last update Sat Jun 25 13:08:16 2016 Nyrandone Noboud-Inpeng
+** Last update Sat Jun 25 14:13:32 2016 Nyrandone Noboud-Inpeng
 */
 
 #include <netinet/in.h>
@@ -60,7 +60,7 @@ t_egg			*new_egg(t_server *srv, t_player *p)
 
 static void		fill_inventory(t_player *new)
 {
-  new->inventory[FOOD] = 1000;
+  new->inventory[FOOD] = 10;
   new->inventory[LINEMATE] = 0;
   new->inventory[DERAUMERE] = 0;
   new->inventory[SIBUR] = 0;
@@ -89,12 +89,10 @@ t_player		*new_player(t_server *srv, t_team *t, t_client *cl)
   new->orientation = rand() % 4 * 90;
   new->id = get_max_player_id(srv) + 1;
   fill_inventory(new);
-  generate_food(srv); // envoyer les cases peut etre
+  generate_food(srv);
   if (create_buffer(&new->buffs) == NULL)
     return (NULL);
   new->buff = NULL;
-  new->x = 0;
-  new->y = 0;
   return (new);
 }
 
